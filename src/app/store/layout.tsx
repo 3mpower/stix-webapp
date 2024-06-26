071d253995b1e8049f37c3e9530f79cdfe5e48dd
Brandon Garvajal <brandon.garvajal@gmail.com>
2024-06-26 11:34:07 +0700

    chore(sticker-storee): change to storeLayout

--- src/app/store/layout.tsx
+++ src/app/store/layout.tsx
@@ -1,10 +1,10 @@
 import Footer from "@/components/footer/footer"
 
-interface FeedLayoutProps {
+interface StoreLayoutProps {
   children: React.ReactNode
 }
 
-export default async function FeedLayout({ children }: FeedLayoutProps) {
+export default async function StoreLayout({ children }: StoreLayoutProps) {
   return (
     <div className="flex min-h-screen flex-col">
       <main className="flex-1">
